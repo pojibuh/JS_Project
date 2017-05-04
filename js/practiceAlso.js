5771d36706eb8b403c56e95458441d66a690fabf
Moktar Jama <moktarjama@gmail.com>
2017-05-04 09:48:44 -0400

    begin adding algo switch functionality to page

--- js/practiceAlso.js
+++ js/practiceAlso.js
@@ -6,14 +6,14 @@ var N = 1 << 0,
     W = 1 << 2,
     E = 1 << 3;
 
-var cellSize = 8,
-    cellSpacing = 8,
+var cellSize = 4,
+    cellSpacing = 4,
     cellWidth = Math.floor((width - cellSpacing) / (cellSize + cellSpacing)),
     cellHeight = Math.floor((height - cellSpacing) / (cellSize + cellSpacing)),
     cells = new Array(cellWidth * cellHeight), // each cell’s edge bits
     frontier = [];
 
-var canvas = d3.select("section").append("canvas")
+var canvas = d3.select("body").append("canvas")
     .attr("width", width)
     .attr("height", height);
 
@@ -36,12 +36,12 @@ frontier.push({index: start, direction: E});
 // Explore the frontier until the tree spans the graph.
 d3.timer(function() {
   var done, k = 0;
-  while (++k < 20 && !(done = exploreFrontier()));
+  while (++k < 50 && !(done = exploreFrontier()));
   return done;
 });
 
 function exploreFrontier() {
-  if ((edge = frontier.pop()) == null) return true;
+  if ((edge = popRandom(frontier)) == null) return true;
 
   var edge,
       i0 = edge.index,
@@ -64,13 +64,10 @@ function exploreFrontier() {
     fillCell(i1);
     cells[i0] |= d0, cells[i1] |= d1;
     context.fillStyle = "blue";
-
-    var m = 0;
-    if (y1 > 0 && cells[i1 - cellWidth] == null) fillSouth(i1 - cellWidth), frontier.push({index: i1, direction: N}), ++m;
-    if (y1 < cellHeight - 1 && cells[i1 + cellWidth] == null) fillSouth(i1), frontier.push({index: i1, direction: S}), ++m;
-    if (x1 > 0 && cells[i1 - 1] == null) fillEast(i1 - 1), frontier.push({index: i1, direction: W}), ++m;
-    if (x1 < cellWidth - 1 && cells[i1 + 1] == null) fillEast(i1), frontier.push({index: i1, direction: E}), ++m;
-    shuffle(frontier, frontier.length - m, frontier.length);
+    if (y1 > 0 && cells[i1 - cellWidth] == null) fillSouth(i1 - cellWidth), frontier.push({index: i1, direction: N});
+    if (y1 < cellHeight - 1 && cells[i1 + cellWidth] == null) fillSouth(i1), frontier.push({index: i1, direction: S});
+    if (x1 > 0 && cells[i1 - 1] == null) fillEast(i1 - 1), frontier.push({index: i1, direction: W});
+    if (x1 < cellWidth - 1 && cells[i1 + 1] == null) fillEast(i1), frontier.push({index: i1, direction: E});
   }
 }
 
@@ -89,13 +86,11 @@ function fillSouth(index) {
   context.fillRect(i * cellSize + (i + 1) * cellSpacing, (j + 1) * (cellSize + cellSpacing), cellSize, cellSpacing);
 }
 
-function shuffle(array, i0, i1) {
-  var m = i1 - i0, t, i, j;
-  while (m) {
-    i = Math.random() * m-- | 0;
-    t = array[m + i0], array[m + i0] = array[i + i0], array[i + i0] = t;
-  }
-  return array;
+function popRandom(array) {
+  if (!array.length) return;
+  var n = array.length, i = Math.random() * n | 0, t;
+  t = array[i], array[i] = array[n - 1], array[n - 1] = t;
+  return array.pop();
 }
 
 d3.select(self.frameElement).style("height", height + "px");
