e8e9ed9fc25722ea292ca62e10f6dc3cb9296f85
Moktar Jama <moktarjama@gmail.com>
2017-05-08 10:58:39 -0400

    complete prim algo

--- js/huntAndKill/primGenerator.js
+++ js/huntAndKill/primGenerator.js
@@ -1,6 +1,6 @@
 class primGenerator {
 
-  constructor() {
+  constructor(maze) {
     this.maze = maze;
     this.grid = maze.grid;
     this.currentCell = this.grid[this.randomize(maze.dimX)][this.randomize(maze.dimY)];
@@ -9,8 +9,21 @@ class primGenerator {
     this.run();
   }
 
-  shuffleFrontier() {
+  randomize(num) {
+    return Math.floor(Math.random() * num)
+  }
 
+  shuffleFrontier() {
+    let i = 0
+    let j = 0
+    let temp = null
+
+    for (i = this.frontier.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1))
+      temp = this.frontier[i]
+      this.frontier[i] = this.frontier[j]
+      this.frontier[j] = temp
+    }
   }
 
   getNeighbors(cell) {
@@ -30,16 +43,41 @@ class primGenerator {
   }
 
   addToFrontier() {
+    let currCellNeighbors = this.getNeighbors(this.currentCell);
+    currCellNeighbors.forEach((neighbor) => {
+      if (!this.frontier.includes(neighbor) && neighbor.visited === false) {
+        this.frontier.push(neighbor);
+      }
+    })
+  }
 
+  render() {
+    let currentGrid = [];
+    this.grid.forEach((row) => {
+      let newRow = [];
+      row.forEach((cell) => {
+        if (cell.visited === true) {
+          newRow.push("X");
+        } else {
+          newRow.push(" ");
+        }
+      })
+      currentGrid.push(newRow);
+    })
+    console.log(currentGrid[0]);
+    console.log(currentGrid[1]);
+    console.log(currentGrid[2]);
+    console.log(currentGrid[3]);
+    console.log("-------------");
   }
 
   run() {
-    let nextCell;
-    while(frontier.length > 0) {
-      shuffleFrontier();
-      nextCell = this.frontier.pop();
-      nextCell.visit();
-      addToFrontier();
-    }
+      while(this.frontier.length > 0) {
+        this.render();
+        this.shuffleFrontier();
+        this.currentCell = this.frontier.pop();
+        this.currentCell.visit();
+        this.addToFrontier();
+      }
   }
 }
