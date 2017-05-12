f41ab524135aabf0276f78ef7e9a677640c084b6
Moktar Jama <moktarjama@gmail.com>
2017-05-12 11:48:50 -0400

    refactor algo setup methods

--- js/entry.js
+++ js/entry.js
@@ -2,7 +2,7 @@ import Maze from "./algos/maze";
 import hkGenerator from "./algos/hkGenerator";
 import primGenerator from "./algos/primGenerator";
 
-function prim() {
+function setupGenerator(generator) {
   let el = document.getElementById("swag");
   el.parentNode.removeChild(el);
   let swagga = document.createElement("div");
@@ -10,28 +10,29 @@ function prim() {
   let hook = document.getElementById("hook");
   hook.appendChild(swagga)
   let n = new Maze (20, 32);
-  let swag = new primGenerator(n);
+
+  if (generator === 'prim') {
+    let swag = new primGenerator(n);
+  } else if (generator === 'hk') {
+    let swag = new hkGenerator(n);
+  }
   swag.run();
 }
 
-function hk() {
-  let el = document.getElementById("swag");
-  el.parentNode.removeChild(el);
-  let swagga = document.createElement("div");
-  swagga.id = "swag";
-  let hook = document.getElementById("hook");
-  hook.appendChild(swagga)
-  let n = new Maze (20, 32);
-  let swag = new hkGenerator(n);
-  swag.run();
-}
+
+// function switchToPrimInfo() {
+//
+// }
+// function switchToPrimInfo() {
+//
+// }
 
 const switchToPrim = () => {
-  document.getElementById("algo").addEventListener( 'click', () => prim() );
+  document.getElementById("algo").addEventListener( 'click', () => setupGenerator('prim') );
 }
 
 const switchToHK = () => {
-  document.getElementById("algo").addEventListener( 'click', () => hk() );
+  document.getElementById("algo").addEventListener( 'click', () => setupGenerator('hk') );
 }
 
 document.getElementById("prim").addEventListener( 'click', () => switchToPrim() );
