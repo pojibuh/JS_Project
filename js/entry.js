accc59d2deffc2925168cdd693385204deaef6e1
Moktar Jama <moktarjama@gmail.com>
2017-05-12 18:18:03 -0400

    make progress on togglin

--- js/entry.js
+++ js/entry.js
@@ -14,24 +14,31 @@ function setupGenerator(generator) {
   if (generator === 'prim') {
     let swag = new primGenerator(n);
   } else if (generator === 'hk') {
+    switchToHKInfo()
     let swag = new hkGenerator(n);
   }
   swag.run();
 }
 
 
-// function switchToPrimInfo() {
-//
-// }
-// function switchToPrimInfo() {
-//
-// }
+function switchToPrimInfo() {
+  let title = document.getElementById("title");
+  title.innerText = "Prim's Algorithm"
+}
+
+function switchToHKInfo() {
+  let title = document.getElementById("title");
+  debugger
+  title.innerText = "Hunt-and-Kill Algorithm"
+}
 
 const switchToPrim = () => {
+  switchToPrimInfo();
   document.getElementById("algo").addEventListener( 'click', () => setupGenerator('prim') );
 }
 
 const switchToHK = () => {
+  switchToHKInfo();
   document.getElementById("algo").addEventListener( 'click', () => setupGenerator('hk') );
 }
 
