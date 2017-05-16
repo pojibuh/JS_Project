e38e08c3f1bd39085917a9c6e88081aab4c2d0f6
Moktar Jama <moktarjama@gmail.com>
2017-05-15 21:41:59 -0400

    remove redundant file

--- js/buttons.js
+++
@@ -1,9 +0,0 @@
-// const switchToPrim = () => {
-//   document.getElementById("swag").src="algos/primGenerator.js"
-//   document.getElementById("algo").setAttribute( "onclick", "prim()" );
-// }
-//
-// const switchToHK = () => {
-//   document.getElementById("swag").src="algos/hkGenerator.js"
-//   document.getElementById("algo").setAttribute( "onclick", "hk()" );
-// }
